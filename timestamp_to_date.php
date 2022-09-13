dba8d5dc79a3b8e90866deaea64e79f5a688527f
Rushi <rushionline@users.noreply.github.com>
2022-09-13 15:46:48 +0530

    upload file timestamp
    uploaded by rushionline

---
+++ timestamp_to_date.php
@@ -0,0 +1,10 @@
+<?php
+echo "timeout_at: ";
+echo date('m/d/Y, h:i:s', 1470998466);
+echo "<br/>";
+echo "expired_at: ";
+echo date('Y-m-d H:i:s', 1471001300);
+echo "<br/>";
+echo date('D-M-Y H:i:s', 1471001300);
+echo "<br/>";
+echo date('d-M-Y H:i:s', 1471001300);
